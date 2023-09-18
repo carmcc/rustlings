18af3547da33769a45133c9c057bbbc57a77c4e2
carmcc <carmine.marchesani@studenti.unicam.it>
2023-09-18 21:47:01 +0200

    [completed] primitive_types

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -5,13 +5,12 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = &a[1..4]; //4 minus 1 is 3, so it slices from index 1 to 3
 
     assert_eq!([2, 3, 4], nice_slice)
 }
